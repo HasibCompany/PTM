1b22bec8812f4fa5c985b56a4168773fd2e7642b
Ibrahim Mousa <ibrahim.mousa@hasib.com.sa>
2023-06-01 08:22:03 +0300

    -

--- Hasib.PTM/Hasib.PTM.Web/src/app/features/ptm/ptm.service.ts
+++ Hasib.PTM/Hasib.PTM.Web/src/app/features/ptm/ptm.service.ts
@@ -1,7 +1,10 @@
+//try councurrent checkouts
+
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Settings } from '@hasib/settings';
+
 const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 @Injectable()
 export class PtmService {
