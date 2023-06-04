eb70e0bf55c237c99faf458051909f2e1e563534
Ibrahim Mousa <ibrahim.mousa@hasib.com.sa>
2023-06-04 14:29:54 +0300

    test

---
+++ Hasib.PTM/Hasib.PTM.API/Controllers/POPermitController.cs
@@ -0,0 +1,47 @@
+﻿using Hasib.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Hasib.PTM.Model;
+using Hasib.PTM.Business;
+
+namespace Hasib.PTM.API.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Produces("application/json")]
+    [Route("api/PTM/POPermit")]
+    [ApiController]
+    public class POPermitController : BaseController
+    {
+        POPermitBL POPermit { get { return new POPermitBL(SessionId, ActionType); } }
+        [HttpGet("LoadPOPermit/{screenCode?}")]
+        public async Task<ActionResult<List<POPermit>>> LoadPOPermit([FromQuery] string screenCode)
+        {
+            var result = await POPermit.LoadPOPermit(screenCode);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return result;
+        }
+        [TypeFilter(typeof(AuthourizationActionFilter), Arguments = new object[] { ActionFlagEnum.CanAdd })]
+        [HttpPost("InsertPOPermit")]
+        public async Task<ActionResult<Output>> InsertPOPermit([FromBody] POPermit obj)
+        {
+            return await POPermit.InsertPOPermit(obj.ScreenCode, obj.PermitFor, obj.UserGroupID, obj.AdministrativePostID, obj.ProfessionID, obj.UserID, obj.FromAmount, obj.ToAmount, obj.FromDate, obj.ToDate, obj.IsActive, obj.CreatedSID);
+        }
+        [TypeFilter(typeof(AuthourizationActionFilter), Arguments = new object[] { ActionFlagEnum.CanUpdate })]
+        [HttpPut("UpdatePOPermit")]
+        public async Task<ActionResult<Output>> UpdatePOPermit([FromBody] POPermit obj)
+        {
+            return await POPermit.UpdatePOPermit(obj.POpermitID, obj.ScreenCode, obj.PermitFor, obj.UserGroupID, obj.AdministrativePostID, obj.ProfessionID, obj.UserID, obj.FromAmount, obj.ToAmount, obj.FromDate, obj.ToDate, obj.IsActive, obj.ModifiedSID, obj.RowStamp);
+        }
+        [TypeFilter(typeof(AuthourizationActionFilter), Arguments = new object[] { ActionFlagEnum.CanDelete })]
+        [HttpPost("DeletePOPermit")]
+        public async Task<ActionResult<Output>> DeletePOPermit([FromBody] POPermit obj)
+        {
+            return await POPermit.DeletePOPermit(obj.POpermitID, obj.RowStamp);
+        }
+    }
+}
---
+++ Hasib.PTM/Hasib.PTM.Business/POPermitBL.cs
@@ -0,0 +1,62 @@
+﻿using Hasib.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hasib.PTM.Model;
+using System;
+
+namespace Hasib.PTM.Business
+{
+    public class POPermitBL : BaseBL
+    {
+        POPermitModel POPermitModel;
+        public POPermitBL(int sessionId, int actionType) : base(sessionId, actionType)
+        {
+            POPermitModel = new POPermitModel(db);
+        }
+        public async Task<List<POPermit>> LoadPOPermit(string screenCode)
+        {
+            try
+            {
+                return await POPermitModel.LoadPOPermit(screenCode);
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
+        public async Task<Output> InsertPOPermit(string screenCode, string permitFor, int? userGroupID, int? administrativePostID, int? professionID, int? userID, decimal fromAmount, decimal toAmount, DateTime? fromDate, DateTime? toDate, bool? isActive, int? createdSID)
+        {
+            try
+            {
+
+                return await POPermitModel.InsertPOPermit(screenCode, permitFor, userGroupID, administrativePostID, professionID, userID, fromAmount, toAmount, fromDate, toDate, isActive, createdSID);
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
+        public async Task<Output> UpdatePOPermit(int? pOpermitID, string screenCode, string permitFor, int? userGroupID, int? administrativePostID, int? professionID, int? userID, decimal fromAmount, decimal toAmount, DateTime? fromDate, DateTime? toDate, bool? isActive, int? modifiedSID, byte[] rowStamp)
+        {
+            try
+            {
+                return await POPermitModel.UpdatePOPermit(pOpermitID, screenCode, permitFor, userGroupID, administrativePostID, professionID, userID, fromAmount, toAmount, fromDate, toDate, isActive, modifiedSID, rowStamp);
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
+        public async Task<Output> DeletePOPermit(int? pOpermitID, byte[] rowStamp)
+        {
+            try
+            {
+                return await POPermitModel.DeletePOPermit(pOpermitID, rowStamp);
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
+    }
+}
