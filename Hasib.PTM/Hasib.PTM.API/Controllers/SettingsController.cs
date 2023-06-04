70fda8b4144ed7f14f8d74d34a89553c21863b28
Towa Zarka <Towa.ZarkaVS@outlook.com>
2023-06-04 11:15:34 +0300

    //37383

--- Hasib.PTM/Hasib.PTM.API/Controllers/SettingsController.cs
+++ Hasib.PTM/Hasib.PTM.API/Controllers/SettingsController.cs
@@ -25,6 +25,7 @@ namespace Hasib.PTM.API.Controllers
         [HttpGet("LoadSettings")]
         public async Task<ActionResult<List<Settings>>> LoadSettings(int? organizationID, string settingCode, int? createdSID)
         {
+            createdSID= SessionId;
             return await Settings.LoadSettings(organizationID, settingCode, createdSID);
         }
         //todo: create a method called UpdateSettingsBulk that takes 1 parametes : settings object and calls UpdateSettingsBulk from the business layer and return output
@@ -34,5 +35,11 @@ namespace Hasib.PTM.API.Controllers
         {
             return await Settings.UpdateSettingsBulk(obj);
         }
+        [HttpGet("CheckAppActivate")]
+        public async Task<ActionResult<bool>> CheckAppActivate(string appCode)
+        {
+            return await Settings.CheckAppActivate(appCode);
+        }
     }
+
 }
